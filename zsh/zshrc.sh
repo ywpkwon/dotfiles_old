d30f793c697fc0b4def91c59fd00d57196f9ef50
Youngwook Paul Kwon <young@berkeley.edu>
2017-10-05 16:18:54 -0700

    general /home/phantom

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -60,6 +60,6 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/e
 export PATH=$PATH:$CUDA_HOME/bin
 
 # virtualenv and virtualenvwrapper
-export WORKON_HOME=/home/paul/.virtualenvs
+export WORKON_HOME=$HOME/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
 
