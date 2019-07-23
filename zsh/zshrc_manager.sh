b712bdfee6a9bdadb6f79ee6957b9b2f4d2b5380
Youngwook Paul Kwon <young@berkeley.edu>
2019-07-23 11:48:00 -0700

    term setting (home/end key in urxvt)

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -9,8 +9,9 @@ else
     else
     	echo "tmux not installed. Run ./deploy to configure dependencies"
     fi
+    echo "Updating configuration"
+    #(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
+    (cd ~/dotfiles && git pull && git submodule update --init --recursive)
 fi
-echo "Updating configuration"
-#(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
-(cd ~/dotfiles && git pull && git submodule update --init --recursive)
+
 source ~/dotfiles/zsh/zshrc.sh
