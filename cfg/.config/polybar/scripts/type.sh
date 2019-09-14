5b155ad42eef24aa46450f3dad98ac10f7f67889
Youngwook Paul Kwon <young@berkeley.edu>
2019-09-14 03:02:16 -0700

    minor

--- cfg/.config/polybar/scripts/type.sh
+++ cfg/.config/polybar/scripts/type.sh
@@ -5,7 +5,7 @@
 
 PDIR="$HOME/.config/polybar"
 LAUNCH="polybar-msg cmd restart"
- 
+
 if  [[ $1 = "-type-1" ]]; then
 # Copying The File
 cp $PDIR/config-t1.ini $PDIR/config.ini
@@ -42,8 +42,14 @@ cp $PDIR/config-t6.ini $PDIR/config.ini
 # Restarting polybar
 $LAUNCH &
 
+elif  [[ $1 = "-type-7" ]]; then
+# Copying The File
+cp $PDIR/config-t7.ini $PDIR/config.ini
+# Restarting polybar
+$LAUNCH &
+
 else
 echo "Available options:
 type-1		type-2		type-3
-type-4		type-5		type-6"
+type-4		type-5		type-6     type-7"
 fi
