01c0b77666dc273ee2d1d714bd0746a11593f11f
Paul Kwon <paulkwon@phantom.ai>
2020-02-03 12:04:37 -0800

    apply fzf ranger_launch i3 to master

--- cfg/.config/nvim/init.vim
+++ cfg/.config/nvim/init.vim
@@ -29,6 +29,8 @@ Plug 'tpope/vim-fugitive'
 Plug 'liuchengxu/vim-which-key'
 Plug 'ianding1/leetcode.vim'
 Plug 'terryma/vim-multiple-cursors'
+Plug 'mhinz/vim-startify'
+Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
 
 call plug#end()
 
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -101,6 +101,7 @@ source ~/dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/dotfiles/zsh/keybindings.sh
 source ~/dotfiles/zsh/grc.zsh
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # Fix for arrow-key searching
 # start typing + [Up-Arrow] - fuzzy find history forward
