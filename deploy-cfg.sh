2ce055e17938bb73eed69c175f0b432f8b4a932c
Paul Kwon <paulkwon@phantom.ai>
2020-02-19 17:57:00 -0800

    rename .config -> config

--- deploy-cfg.sh
+++ deploy-cfg.sh
@@ -3,23 +3,23 @@ if [ ! -d "$HOME/.config" ]; then
     echo "mkdir ~/.config"
     mkdir $HOME/.config
 fi
-ln -sf ~/dotfiles/cfg/.config/i3 ~/.config/
-ln -sf ~/dotfiles/cfg/.config/conky ~/.config/
-ln -sf ~/dotfiles/cfg/.config/rofi ~/.config/
-ln -sf ~/dotfiles/cfg/.config/polybar ~/.config/
-ln -sf ~/dotfiles/cfg/.config/compton.conf ~/.config/
-ln -sf ~/dotfiles/cfg/.config/dunst ~/.config/
-ln -sf ~/dotfiles/cfg/.config/alacritty ~/.config/
-ln -sf ~/dotfiles/cfg/.config/nvim ~/.config/
-ln -sf ~/dotfiles/cfg/.config/ranger ~/.config/
-ln -sf ~/dotfiles/cfg/.config/kitty ~/.config/
+ln -sf ~/dotfiles/cfg/config/i3 ~/.config/
+ln -sf ~/dotfiles/cfg/config/rofi ~/.config/
+ln -sf ~/dotfiles/cfg/config/polybar ~/.config/
+ln -sf ~/dotfiles/cfg/config/compton.conf ~/.config/
+ln -sf ~/dotfiles/cfg/config/dunst ~/.config/
+ln -sf ~/dotfiles/cfg/config/alacritty ~/.config/
+ln -sf ~/dotfiles/cfg/config/nvim ~/.config/
+ln -sf ~/dotfiles/cfg/config/ranger ~/.config/
+ln -sf ~/dotfiles/cfg/config/kitty ~/.config/
+ln -sf ~/dotfiles/cfg/urxvt ~/.urxvt
+ln -sf ~/dotfiles/cfg/wallpapers ~/.wallpapers
 ln -sf ~/dotfiles/cfg/.fonts ~
 ln -sf ~/dotfiles/cfg/.Xresources ~
-ln -sf ~/dotfiles/cfg/.urxvt ~
-ln -sf ~/dotfiles/cfg/.wallpapers ~
 ln -sf ~/dotfiles/cfg/.notes.txt ~
 ln -sf ~/dotfiles/cfg/.conky ~
-ln -sf ~/pCloudDrive/okular/docdata ~/.kde/share/apps/okular/
+#ln -sf ~/pCloudDrive/okular/docdata ~/.kde/share/apps/okular/
+#ln -sf ~/dotfiles/cfg/config/conky ~/.config/
 # rsync -rav ~/dotfiles/cfg/.fonts/* ~/.fonts/
 
 echo '---------------- sync done ----------------'
